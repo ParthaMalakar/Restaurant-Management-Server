e45305e1eade749acdeb9fbfd23d746c549254be
ParthaMalakar <parthamalakar4321@gmail.com>
2023-11-08 00:05:01 +0600

    token created and send client success true and save the token in cookie

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+  origin: [
+      'http://localhost:5173'
+      
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.noswvlt.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,6 +33,21 @@ async function run() {
 
     const foodsCollection = client.db('FoodDB').collection('FoodItems');
     const userCollection = client.db('FoodDB').collection('user');
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    }) .send({ success: true });
+
+    })
+
+
+
     app.get('/foodsItem', async (req, res) => {
      
       const result = await foodsCollection.find()
